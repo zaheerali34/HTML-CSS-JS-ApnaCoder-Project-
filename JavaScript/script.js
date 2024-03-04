7f712b1093c56f631ee6cd12e1c8130d4504418e
Zaheer Rahman <Zaheerrahman0@gmail.com>
2024-03-04 01:36:07 -0800

    UpData 3

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -4,3 +4,17 @@ const nav_bar = document.querySelector(".nav_bar");
 icon.addEventListener("click", ()=>{
     nav_bar.classList.toggle("nav");
 })
+
+// ........... Swiper ...........
+
+let swiper = new Swiper(".mySwiper", {
+    slidesPerView: 4,
+    spaceBetween: 25,
+    freeMode: true,
+    
+    cssMode: true,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+});
